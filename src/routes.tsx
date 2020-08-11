7ad63fc9384e8f4f3868bf1bf8ba48defa55e6e8
Pietro <pietro.souza@itriad.org.br>
2020-08-11 09:16:25 -0400

    simulating route guard

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import { isAuthenticated } from './auth';
 import DragonsForm from './pages/DragonsForm';
 import DragonsList from './pages/DragonsList';
+
+const PrivateRoute: React.FC<any> = ({ component: Component, ...rest }) => (
+    <Route
+        {...rest}
+        render={props =>
+            isAuthenticated() ? (
+                <Component {...props} />
+            ) : (
+                    <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+                )
+        }
+    />
+);
 
 function Routes() {
     return (
         <BrowserRouter>
-            <Route path="/" exact component={DragonsList}></Route>
-            <Route path="/dragon-form/:id?" component={DragonsForm}></Route>
+            <Switch>
+                {/* <Route path="/" exact component={DragonsList}></Route> */}
+                <PrivateRoute exact path='/' component={DragonsList}></PrivateRoute>
+                <PrivateRoute path="/dragon-form/:id?" component={DragonsForm}></PrivateRoute>
+            </Switch>
         </BrowserRouter>
     )
 }
