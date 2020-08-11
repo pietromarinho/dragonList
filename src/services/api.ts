fbe4d5cca05f2f71e7cd1705429b8b8035cac004
Pietro <pietro.souza@itriad.org.br>
2020-08-11 11:38:58 -0400

    cors anywhere

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+const corsAnyWhere: string = 'https://cors-anywhere.herokuapp.com/';
 
 const api = axios.create({
-    baseURL: 'http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon'
+    baseURL: corsAnyWhere + 'http://5c4b2a47aa8ee500142b4887.mockapi.io/api/v1/dragon'
 });
 
 export default api;
